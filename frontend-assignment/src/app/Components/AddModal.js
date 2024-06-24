fbed4cf352b126725be7bfd5a48a6f4654d97024
HannieDew <wong.lihan48@gmail.com>
2024-06-25 01:28:44 +0800

    added responsive design

---
+++ frontend-assignment/src/app/Components/AddModal.js
@@ -0,0 +1,146 @@
+// Modal to allow user to add new data entries
+import { useState, useContext } from "react";
+import DataContext from "../Context/DataContext";
+
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Modal from "@mui/material/Modal";
+
+// To style the modal
+const style = {
+  position: "absolute",
+  top: "40%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: "90%", // Default width for mobile screens
+  maxWidth: "350px", // Max width for small mobile screens
+  bgcolor: "background.paper",
+  border: "1px solid #000",
+  borderRadius: 3,
+  boxShadow: 24,
+  p: 4,
+  // Media query for desktop screens
+  "@media (min-width: 768px)": {
+    width: "50%", // Adjusted width for desktop screens
+    maxWidth: "500px", // Max width for desktop screens
+  },
+};
+
+function AddModal() {
+  const { data, addData } = useContext(DataContext);
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => {
+    setOpen(false);
+    // Clear input fields on modal close
+    setName("");
+    setEmail("");
+    setPhone("");
+    setErrors({}); // Reset errors object
+  };
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+
+  const [errors, setErrors] = useState({});
+
+  const handleAddData = () => {
+    let errors = {};
+
+    // Name validation (check length)
+    if (name.trim().length < 5) {
+      errors.name = "Name must be at least 5 characters";
+    }
+
+    // Email validation (check for @, .com and spaces)
+    if (
+      !email.includes("@") ||
+      !email.endsWith(".com") ||
+      email.includes(" ")
+    ) {
+      errors.email = "Enter a valid email address (e.g., user@example.com)";
+    }
+
+    // Phone vailidation (check for +)
+    if (!phone.startsWith("+")) {
+      errors.phone = "Phone number must start with '+'";
+    }
+
+    // If there are errors, update state and prevent data addition
+    if (Object.keys(errors).length > 0) {
+      setErrors(errors);
+      return;
+    }
+
+    // If no errors, add data
+    const newData = {
+      id: data.length + 1, // Generate a new ID
+      name: name,
+      email: email,
+      phone: phone,
+    };
+    addData(newData);
+    setErrors({}); // delete all errors since its valid
+  };
+
+  return (
+    <div>
+      {/* Button to open modal */}
+      <Button variant="contained" onClick={handleOpen}>
+        Add User
+      </Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <div
+            style={{ display: "flex", flexDirection: "column", gap: "16px" }}
+          >
+            <TextField
+              label="Name"
+              variant="outlined"
+              size="small"
+              fullWidth
+              id="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              error={errors.name !== undefined}
+              helperText={errors.name}
+            />
+            <TextField
+              label="Email"
+              variant="outlined"
+              size="small"
+              fullWidth
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              error={errors.email !== undefined}
+              helperText={errors.email}
+            />
+            <TextField
+              label="Phone"
+              variant="outlined"
+              size="small"
+              fullWidth
+              id="phone"
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)}
+              error={errors.phone !== undefined}
+              helperText={errors.phone}
+            />
+            <Button variant="contained" onClick={handleAddData} sx={{ mt: 2 }}>
+              Add
+            </Button>
+          </div>
+        </Box>
+      </Modal>
+    </div>
+  );
+}
+
+export default AddModal;
--- frontend-assignment/src/app/Components/Table.js
+++ frontend-assignment/src/app/Components/Table.js
@@ -1,45 +1,60 @@
 import { useContext } from "react";
 import DataContext from "../Context/DataContext";
-import { Button } from "@mui/material";
+import AddModal from "./AddModal";
 
 function Table() {
-  const { data, addData } = useContext(DataContext);
+  const { data } = useContext(DataContext);
 
-  const handleAddData = () => {
-    const newData = {
-      id: data.length + 1, // Generate a new ID
-      name: "New User",
-      email: "newuser@example.com",
-      phone: "555-555-5555",
-    };
-    addData(newData);
-  };
+  // For mobile: render cards
+  // For desktop: render table
+
   return (
-    <div className="text-center mt-24">
-      <h1 className="mb-8 text-2xl font-bold">User Table</h1>
-      <Button variant="contained" onClick={handleAddData}>
-        Add User
-      </Button>
-      <table className="mx-auto w-full max-w-4xl mt-8">
-        <thead>
-          <tr>
-            <th className="border px-4 py-2"># ID</th>
-            <th className="border px-4 py-2">Name</th>
-            <th className="border px-4 py-2">Email</th>
-            <th className="border px-4 py-2">Phone</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data.map((item) => (
-            <tr key={item.id}>
-              <td className="border px-4 py-2">{item.id}</td>
-              <td className="border px-4 py-2">{item.name}</td>
-              <td className="border px-4 py-2">{item.email}</td>
-              <td className="border px-4 py-2">{item.phone}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+    <div className="text-center mt-8 px-4 md:px-0">
+      <h1 className="mb-4 text-2xl font-bold">User Table</h1>
+      <AddModal />
+      {/* Responsive layout using Tailwind CSS */}
+      <div className="lg:block hidden">
+        {" "}
+        {/* For desktop */}
+        <div className="overflow-x-auto mb-20">
+          <table className="mx-auto w-full max-w-4xl mt-8">
+            <thead>
+              <tr className="bg-gray-100">
+                <th className="border px-4 py-2"># ID</th>
+                <th className="border px-4 py-2">Name</th>
+                <th className="border px-4 py-2">Email</th>
+                <th className="border px-4 py-2">Phone</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.map((item) => (
+                <tr key={item.id}>
+                  <td className="border px-4 py-2">{item.id}</td>
+                  <td className="border px-4 py-2">{item.name}</td>
+                  <td className="border px-4 py-2">{item.email}</td>
+                  <td className="border px-4 py-2">{item.phone}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+      <div className="lg:hidden">
+        {" "}
+        {/* For mobile */}
+        <div className="text-center mt-8 px-4 md:px-0 mb-10">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-4">
+            {data.map((item) => (
+              <div key={item.id} className="bg-white rounded-lg shadow-md p-4">
+                <p className="text-lg font-semibold mb-2">#{item.id}</p>
+                <p className="text-base mb-2">{item.name}</p>
+                <p className="text-base mb-2">{item.email}</p>
+                <p className="text-base">{item.phone}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
